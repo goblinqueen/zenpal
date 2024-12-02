e16a1ef20084e3ec52ad313b1c555c80a008d43e
Eltha Black <eltha@yandex.ru>
2024-12-02 11:28:43 +0200

    other account id

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from config import *
 
 OP_FAMILY_ID = '50ca9746-f13a-4b67-adbb-1fe8f7f28439'
 OP_EUR_ID = 'f02f21c3-2686-4c78-a3da-cc4c776fba93'
+OP_MONEY_BOX = '052b718d-74f1-4e25-b1fa-5f7b9e7a7ca4'
 
 from op import OPReader
 import zenmoney
@@ -36,6 +37,7 @@ def get_updates(zen, _filename, _acc_id):
     for line in op.read():
         def check(_zen, _op):
 
+
             _f = ['date']  # Fields to compare (all must match)
             if _op['income'] > 0:
                 _f += ['income']
@@ -47,6 +49,9 @@ def get_updates(zen, _filename, _acc_id):
             for _n in _f:
                 if not _zen.get(_n, None) == _op.get(_n, None):
                     return False
+
+            if _zen.get('deleted'):
+                print(_op)
             return True
 
         search = [x for x in zen.transaction if check(x, line)]
@@ -67,7 +72,7 @@ def get_updates(zen, _filename, _acc_id):
                 'opIncomeInstrument': None,
                 'opOutcomeInstrument': None,
                 'latitude': None,
-                'longitude': None
+                'longitude': None,
             })
             diff['transaction'].append(line)
     return diff
@@ -79,15 +84,18 @@ def main():
     filename = 'zenmoney.json'
 
     zen = load_or_sync(filename, token)
-    in_dates = '20241015-20241115'
+    in_dates = '20240901-20241126'
 
     op_files = [
-        (f'C:\\Users\\eltha\\Downloads\\tapahtumat{in_dates}.csv', OP_EUR_ID),
-        (f'C:\\Users\\eltha\\Downloads\\tapahtumat{in_dates} (1).csv', OP_FAMILY_ID)
+        # (f'/Users/eltha/Downloads/tapahtumat{in_dates}.csv', OP_EUR_ID),
+        (f'/Users/eltha/Downloads/tapahtumat{in_dates} (1).csv', OP_FAMILY_ID),
+        # (f'/Users/eltha/Downloads/tapahtumat{in_dates} (2).csv', OP_MONEY_BOX),
     ]
 
+    # print([x for x in zen.account if 'OP M' in x['title']])
 
     for op_file in op_files:
+        print()
         print(zen.get_by_value('account', 'id', op_file[1])[0]['title'])
         diff = get_updates(zen, op_file[0], op_file[1])
         print(f'Updating {len(diff["transaction"])} items')
